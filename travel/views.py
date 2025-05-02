c815bfacf4c271613bb22e866fb3af0de9e7efb3
michellejtan <michelle0223@gmail.com>
2025-05-02 15:54:02 -0700

    wip: import all models, define trip_index

--- travel/views.py
+++ travel/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
+from .models import Trip, Itinerary, PackingItem
 
 
 # Create your views here.
@@ -10,4 +11,10 @@ def about(request):
      contact_details = 'you can reach support at support@catcollector.com' 
      return render(request, 'about.html', {
         'contact': contact_details
+    })
+
+def trip_index(request):
+    trips = Trip.objects.filter(user=request.user)
+    return render(request, 'trips/index.html', {
+        'trips': trips
     })

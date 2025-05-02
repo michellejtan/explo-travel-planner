05c35bbf5ad1f0f85a17d29f0c0dbfc0078ca920
michellejtan <michelle0223@gmail.com>
2025-05-02 15:21:53 -0700

    wip: home path to use the new home CBV

--- travel/urls.py
+++ travel/urls.py
@@ -3,6 +3,6 @@ from . import views # Import views to connect routes to view functions
 
 urlpatterns = [
     # Routes will be added here
-    path('', views.home, name='home'),
+    path('', views.Home.as_view(), name='home'),
     path('about/', views.about, name='about'),
 ]

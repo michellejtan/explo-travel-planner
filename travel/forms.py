7ad5524957041cc550f8e10bab7f5c9fb1a68a2e
michellejtan <michelle0223@gmail.com>
2025-05-05 13:58:02 -0700

    Create the ModelForm for the Itinerary Model

---
+++ travel/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Itinerary
+
+class ItineraryForm(forms.ModelForm):
+    class Meta:
+        model = Itinerary
+        fields = ['date', 'time', 'type', 'activity', 'address', 'description']
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'}),
+            'time': forms.TimeInput(attrs={'type': 'time'}),
+            'description': forms.Textarea(attrs={'rows': 3}),
+        }

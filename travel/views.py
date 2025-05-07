81ac48d8c71eb997e80715c2d97d884a6040f3a4
michellejtan <michelle0223@gmail.com>
2025-05-06 19:49:59 -0700

    wip: import login & UserCreationForm & create the signup view function

--- travel/views.py
+++ travel/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
 from .models import Trip, Itinerary, PackingItem
 from .forms import ItineraryForm
@@ -12,6 +14,21 @@ from datetime import date
 # Create your views here.
 class Home(LoginView):
     template_name = 'home.html'
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('trip-index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'signup.html', context)
+
 
 def about(request):
      contact_details = 'you can reach support at support@explo.com' 
